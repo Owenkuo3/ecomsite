1cabdaa6d1b4c15795f3d81294ad41da85dd1a7f
Owen Kuo (GMI) <owen.kuo@gmimarkets.com>
2023-11-17 18:50:51 +0800

    Update ALLOWED_HOSTS

--- ecomsite/settings.py
+++ ecomsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-fljt6hw5smw9-@&9gnh7a^zd)zt-#dk+#b7jpfba9$l_v1&9ir
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['owenkuo3.github.io',]
+ALLOWED_HOSTS = ['owenkuo3.github.io','ecomsite-a2506b9eeda5.herokuapp.com',]
 
 
 # Application definition
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
